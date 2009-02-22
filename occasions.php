f19f08f0f094866c70f1a96879f01e824c10f06e
Alphawolf <Alphawolf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-22 23:58:17 +0000

    Version update 1.0
    git-svn-id: https://plugins.svn.wordpress.org/occasions/trunk@96718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- occasions.php
+++ occasions.php
@@ -25,6 +25,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+/**
+ * The main plugin file
+ *
+ * @package WordPress_Plugins
+ * @subpackage Occasions
+ */
+ 
 
 /**
  * Pre-2.6 compatibility
